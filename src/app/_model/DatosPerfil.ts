8c56a96077d122a32c7dd733e76088f5c3ddcf82
Elsebitas <tibion1048@gmail.com>
2021-05-10 17:27:45 -0500

    Correccion login y se agrego el perfil

---
+++ src/app/_model/DatosPerfil.ts
@@ -0,0 +1,16 @@
+/**
+ * Clase que mapea el servicio de getHotelesDestacados
+ */
+export class DatosPerfil {
+    id: number;
+    nombre: string;
+    apellido: string;
+    correo: string;
+    telefono: string;
+    usuario: string;
+    contrasena: string;
+    idestado: number;
+    fotoperfil: string;
+    mensaje: string;
+    url: string;
+}
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -2,6 +2,7 @@ import { environment } from './../../../environments/environment';
 import { PerfilService } from './../../_service/perfil.service';
 import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { DatosPerfil } from 'src/app/_model/DatosPerfil';
 
 @Component({
   selector: 'app-perfil',
@@ -13,13 +14,20 @@ export class PerfilComponent implements OnInit {
   constructor(private perfilService: PerfilService) { }
 
   ngOnInit(): void {
+    this.postCargarDatosPerfil();
   }
 
   postCargarDatosPerfil(){
+    console.log("entro!!");
+    
     const helper = new JwtHelperService();
-    const decodedToken = helper.decodeToken(environment.TOKEN);
+    const decodedToken = helper.decodeToken(sessionStorage.getItem(environment.TOKEN));
     console.log(decodedToken.name);
-    this.perfilService.postCargarDatosPerfil(decodedToken).subscribe(data =>{
+
+    let datosPerfil: DatosPerfil;
+    datosPerfil = new DatosPerfil();
+    datosPerfil.usuario = decodedToken.name;
+    this.perfilService.postCargarDatosPerfil(datosPerfil).subscribe(data =>{
       console.log(data);
     })
   }
